5a509dcebb58fd43def63dca255af20dcac87a78
De1pl <sa.bystrov2004@mail.ru>
2023-02-18 22:38:21 +0300

    con

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-'''
-import imapclient
-import pprint
-imapObj = imapclient.IMAPClient('imap.mail.ru', ssl=True)
-imapObj.login('mail', 'password')
-pprint.pprint(imapObj.list_folders())
-print(imapObj.select_folder('INBOX/2FA'))
-print(imapObj.search('UNSEEN'))'''
-
 import smtplib
-# Подключение к серверу и настройка TLS шифрования
-smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
-smtpObj.ehlo()
-smtpObj.starttls()
-# Вход в учетную запись
-smtpObj.login('почта', 'пароль')
-# Отправка письма
-smtpObj.sendmail('от кого отправить', 'куда отправить', 'Сообщение')
-# Разрыв соединения
-smtpObj.quit()
+def OtpravkaMail():
+    '''Отправление сообщения на почту'''
+    # Подключение к серверу и настройка TLS шифрования
+    smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    # Вход в учетную запись
+    smtpObj.login('почта', 'пароль')
+    # Отправка письма
+    smtpObj.sendmail('от кого отправить', 'куда отправить', 'Сообщение')
+    # Разрыв соединения
+    smtpObj.quit()
+
+def
